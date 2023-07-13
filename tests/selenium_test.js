003d013cd4aa005534e8b60d132b441e8fe74757
Sebastian Piazza <sebastian@adermarkarian23gmail.onmicrosoft.com>
2023-07-13 17:54:44 -0300

    update selenium tests

--- tests/selenium_test.js
+++ tests/selenium_test.js
@@ -21,14 +21,13 @@ const { Builder, By, Key } = require("selenium-webdriver");
     logging.installConsoleHandler()
 
     //const deployedURL = 
-    const deployedURL =process.argv[2]; // process.env.DEPLOYED_URL; // 'http://bucket-fe-angular-dev.s3-website-us-east-1.amazonaws.com/index.html';
+    const deployedURL =process.argv[2]; // get url as parameter 'http://bucket-fe-angular-dev.s3-website-us-east-1.amazonaws.com/index.html';
     await driver.get(deployedURL);
-
 
     var elements = ['pepe'];
     const currentDate = new Date();
     // Sleep until the div we want is visible or 5 seconds is over
-    var end = currentDate.getMilliseconds() + 15000;
+    var end = currentDate.getMilliseconds() + 10000;
     while (currentDate.getMilliseconds() < end) {
         elements = await driver.findElements(By.tagName('a'));
 
@@ -37,6 +36,8 @@ const { Builder, By, Key } = require("selenium-webdriver");
           break;
         }
     }
+
+    assert.notEqual(elements.length,0);
 
      // Iterate over the elements
      for (const element of elements) {
@@ -59,9 +60,13 @@ const { Builder, By, Key } = require("selenium-webdriver");
       }
     }
 
-      return driver;
+     // return driver;
     // Add more test actions as needed
-  } finally {
+  }
+ catch (error) {
+  console.error('Error running Selenium test:', error);
+  assert.fail('Assertion failed');
+} finally {
     await driver.quit();
   }
 })();
